f406c4f9cc9bdca199ee57bbdc97e03af9105683
Andrew Lamchenko <31867646+smartboxchannel@users.noreply.github.com>
2024-04-01 22:59:41 +0300

    Update EFEKTA_T8_R2.js

--- z2m_Converter/EFEKTA_T8_R2.js
+++ z2m_Converter/EFEKTA_T8_R2.js
@@ -10,10 +10,8 @@ const tz = require('zigbee-herdsman-converters/converters/toZigbee');
 const exposes = require('zigbee-herdsman-converters/lib/exposes');
 const constants = require('zigbee-herdsman-converters/lib/constants');
 const reporting = require('zigbee-herdsman-converters/lib/reporting');
-const extend = require('zigbee-herdsman-converters/lib/extend');
 const e = exposes.presets;
 const ea = exposes.access;
-//const {calibrateAndPrecisionRoundOptions} = require('zigbee-herdsman-converters/lib/utils');
 const {postfixWithEndpointName} = require('zigbee-herdsman-converters/lib/utils');
 
 
